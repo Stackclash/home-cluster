d9185d1ddd2f541df457136e0121d89879347593
Richard Cox <6799456+RickCoxDev@users.noreply.github.com>
2020-12-27 17:57:26 -0600

    Changin setup script

--- setup/setup.sh
+++ setup/setup.sh
@@ -2,11 +2,7 @@
 
 REPO_ROOT=$(git rev-parse --show-toplevel)
 
-need() {
-    which "$1" &>/dev/null || die "Binary '$1' is missing but required"
-}
-
-need "kubectl"
+. ${REPO_ROOT}/setup/.env
 
 message() {
   echo -e "\n######################################################################"
@@ -14,24 +10,39 @@ message() {
   echo "######################################################################"
 }
 
-message "installing flux"
-flux install \
---version=latest \
---components=source-controller,kustomize-controller,helm-controller,notification-controller \
---namespace=flux-system \
---network-policy=false \
---arch=arm
+need() {
+    which "$1" &>/dev/null || die "Binary '$1' is missing but required"
+}
 
-message "adding CRDs"
-kubectl apply -f "${REPO_ROOT}"/crds
+need kubectl
+need flux
 
-message "adding helm repositories"
-kubectl apply -f "${REPO_ROOT}"/deployments/flux-system/helm-repositories
+if [ -z "$GITHUB_TOKEN" ]; then
+  echo "GITHUB_TOKEN is not set! Check setup/.env"
+  exit 1
+fi
 
-message "installing sealed-secrets"
-kubectl apply -f "${REPO_ROOT}"/.secrets/sealed-secrets/my-sealed-secrets-key.yml
-kubectl apply -f "${REPO_ROOT}"/deployments/kube-system/sealed-secrets/sealed-secrets.yml
+message "checking fluxv2 prerequisites"
+flux check --pre
+[[ $? -ne 0 ]] && echo "Prerequisites were not satisfied" && exit 1
 
-message "setting up git repository"
-kubectl apply -f "${REPO_ROOT}"/.secrets/flux-system/ssh-credentials.yml
-kubectl apply -f "${REPO_ROOT}"/deployments/flux-system/gotk-sync.yaml
+# Adding the CRDs is necessary because these custom resources live in the github repo
+# message "adding CRDs"
+# kubectl apply -f "${REPO_ROOT}"/crds
+
+# Adding the system-upgrade controller because the crd doesn't work without it
+message "adding system-upgrade controller"
+kubectl apply -f deployment/system-upgrade/system-upgrade-controller.yml
+kubectl wait --for=condition=ready pod -n system-upgrade -l upgrade.cattle.io/controller=system-upgrade-controller
+
+message "adding personal sealed-secrets key"
+kubectl apply -f .secrets/sealed-secrets/my-sealed-secrets-key.yml
+
+message "installing flux components"
+flux bootstrap github \
+  --owner=RickCoxDev \
+  --repository=k3s-gitops \
+  --branch=master \
+  --path=deployments \
+  --personal \
+  --arch=arm
