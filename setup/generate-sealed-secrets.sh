231abddcef50359e2c7166858881de8401fa132e
Richard Cox <6799456+RickCoxDev@users.noreply.github.com>
2020-12-29 15:13:35 -0600

    Fixing Typo

--- setup/generate-sealed-secrets.sh
+++ setup/generate-sealed-secrets.sh
@@ -30,10 +30,10 @@ then
         if [[ "${file}" =~ "values" ]]
         then
             echo "[*] Generating helm secret '${secret_name}' in namespace '${namespace}'..."
-            secret_json=$(kubectl -n "${namespace}" create secret generic "${secret_name}" --from-literal=values.yaml="$contents" --dry-run=client -o json)
+            secret_json=$(kubectl -n "${namespace}" create secret generic "${namespace}-helm-values" --from-literal=values.yaml="$contents" --dry-run=client -o json)
         else
             echo "[*] Generating generic secret '${secret_name}' in namespace '${namespace}'..."
-            secret_json=$(kubectl -n "${namespace}" create secret generic "${namespace}-helm-values" --from-literal="$contents" --dry-run=client -o json)
+            secret_json=$(kubectl -n "${namespace}" create secret generic "${secret_name}" --from-literal="$contents" --dry-run=client -o json)
         fi
 
         echo "${secret_json}" |
